f85cb2c5c57bfd392551cbd2d850b8bce98ea8de
aliaksandr <aleksandr.dudarenko@gmail.com>
2023-11-30 14:49:50 +0300

    create custom scope

--- app/src/main/java/com/example/dependencyinjectionapptest/example2/data/database/ExampleDatabase.kt
+++ app/src/main/java/com/example/dependencyinjectionapptest/example2/data/database/ExampleDatabase.kt
@@ -3,23 +3,23 @@ package com.example.dependencyinjectionapptest.example2.data.database
 import android.content.Context
 import android.util.Log
 import com.example.dependencyinjectionapptest.R
+import com.example.dependencyinjectionapptest.example2.di.ApplicationScope
 import javax.inject.Inject
 import javax.inject.Singleton
 
-@Singleton
 class ExampleDatabase @Inject constructor(
-	private val context: Context,
-	private val currentTimeMillis: Long
+    private val context: Context,
+    private val currentTimeMillis: Long
 ) {
 
-	fun method() {
-		Log.d(
-			TAG_NAME,
-			"Example database ${context.getString(R.string.app_name)}, $currentTimeMillis, $this"
-		)
-	}
+    fun method() {
+        Log.d(
+            TAG_NAME,
+            "Example database ${context.getString(R.string.app_name)}, $currentTimeMillis, $this"
+        )
+    }
 
-	companion object {
-		private const val TAG_NAME = "EXAMPLE"
-	}
+    companion object {
+        private const val TAG_NAME = "EXAMPLE"
+    }
 }
--- app/src/main/java/com/example/dependencyinjectionapptest/example2/data/network/ExampleApiService.kt
+++ app/src/main/java/com/example/dependencyinjectionapptest/example2/data/network/ExampleApiService.kt
@@ -3,10 +3,10 @@ package com.example.dependencyinjectionapptest.example2.data.network
 import android.content.Context
 import android.util.Log
 import com.example.dependencyinjectionapptest.R
+import com.example.dependencyinjectionapptest.example2.di.ApplicationScope
 import javax.inject.Inject
 import javax.inject.Singleton
 
-@Singleton
 class ExampleApiService @Inject constructor(
 	private val context: Context,
 	private val currentTimeMillis: Long
