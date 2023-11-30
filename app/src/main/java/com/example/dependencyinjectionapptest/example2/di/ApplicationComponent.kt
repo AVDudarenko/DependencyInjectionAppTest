efa7667214eeef6eed99ebeac45ae0f73e529110
Aliaksandr <dyvedd@gmail.com>
2023-11-30 12:56:42 +0300

    add singleton for objects, add factory component

--- app/src/main/java/com/example/dependencyinjectionapptest/example2/di/ApplicationComponent.kt
+++ app/src/main/java/com/example/dependencyinjectionapptest/example2/di/ApplicationComponent.kt
@@ -4,23 +4,20 @@ import android.content.Context
 import com.example.dependencyinjectionapptest.example2.presentation.MainActivity
 import dagger.BindsInstance
 import dagger.Component
+import javax.inject.Singleton
 
+@Singleton
 @Component(modules = [DataModule::class, DomainModule::class])
 interface ApplicationComponent {
 
 	fun inject(activity: MainActivity)
 
-	@Component.Builder
-	interface ApplicationComponentBuilder {
-
-		@BindsInstance
-		fun context(context: Context): ApplicationComponentBuilder
-
-		@BindsInstance
-		fun currentTime(currentTimeMillis: Long): ApplicationComponentBuilder
-
-		fun build(): ApplicationComponent
-
+	@Component.Factory
+	interface ApplicationComponentFactory {
+		fun create(
+			@BindsInstance context: Context,
+			@BindsInstance currentTimeMillis: Long
+		): ApplicationComponent
 	}
 
 }
