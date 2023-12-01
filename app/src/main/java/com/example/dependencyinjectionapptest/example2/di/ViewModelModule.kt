93f9369524675cac2836682721456a7bfae40edd
Aliaksandr <dyvedd@gmail.com>
2023-12-01 12:54:00 +0300

    create annotation for view model key

--- app/src/main/java/com/example/dependencyinjectionapptest/example2/di/ViewModelModule.kt
+++ app/src/main/java/com/example/dependencyinjectionapptest/example2/di/ViewModelModule.kt
@@ -6,19 +6,18 @@ import com.example.dependencyinjectionapptest.example2.presentation.ExampleViewM
 import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
-import dagger.multibindings.StringKey
 
 @Module
 interface ViewModelModule {
 
-    @IntoMap
-    @StringKey("ExampleViewModel")
-    @Binds
-    fun bindExampleViewModel(impl: ExampleViewModel): ViewModel
+	@IntoMap
+	@ViewModelKey(ExampleViewModel::class)
+	@Binds
+	fun bindExampleViewModel(impl: ExampleViewModel): ViewModel
 
-    @IntoMap
-    @StringKey("ExampleViewModel2")
-    @Binds
-    fun bindExampleViewModel2(impl: ExampleViewModel2): ViewModel
+	@IntoMap
+	@ViewModelKey(ExampleViewModel2::class)
+	@Binds
+	fun bindExampleViewModel2(impl: ExampleViewModel2): ViewModel
 
 }
